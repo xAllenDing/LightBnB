881f769b7363c5b7a492d09a6d84ec5d2bbb6894
Allen Ding <allen.ding@hotmail.com>
2023-02-11 19:19:21 -0800

    refactor reservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -74,8 +74,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return pool.query(`SELECT reservations.*, properties.*
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  LIMIT $2`, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message)
+    });
+};
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
